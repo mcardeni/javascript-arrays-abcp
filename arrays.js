983e9ef8cb41b7ebbc22de370e6fcf1fd4348847
cardenio@udel.edu <cardenio@udel.edu>
2018-01-22 03:57:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,7 +23,7 @@ function accessElementInArray (array,index) {
 return array[index];
 }
 
-function destructivelyRemoveElementToBeginningOfArray (array,element) {
+function destructivelyRemoveElementFromBeginningOfArray (array,element) {
 array.shift(element);
 return array;
 }
