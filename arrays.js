7dd39ccdeba9e9f3e566c52082acb683a4591a6c
cardenio@udel.edu <cardenio@udel.edu>
2018-01-22 03:49:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,21 +4,3 @@ var cities = ["New York", "San Francisco"]
 function addElementToBeginningOfArray (array,element) {
 return [element, ...array];
 }
-function destructivelyRemoveElementToBeginningOfArray(array,element){ 
-  var array1 = array
-  array.slice(element);
-  return array;
-}
-function addElementToEndOfArray (array,element) {
-return [...array, element];
-}
-
-function destructivelyAddElementToEndOfArray(array,element){ 
-  array.push(element);
-  return array;
-}
-
-function accessElementInArray(array, index){
-  var array1 = array
-  return array1[index];
-}
